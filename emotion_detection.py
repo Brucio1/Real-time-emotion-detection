5d6ab13b1e03eeb826434cc9c1270a78e5a6e6a3
Brucio1 <70476910+Brucio1@users.noreply.github.com>
2020-09-07 13:26:53 +0100

    Add files via upload

---
+++ emotion_detection.py
@@ -0,0 +1,54 @@
+import cv2
+from tensorflow.keras.models import load_model
+import numpy as np
+
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+model = load_model('emotion_model_new.h5')
+
+def get_prediction(face_image):
+	#Resize and reshape to a (1, 48, 48, 1) for Conv2D on the model
+	image = cv2.resize(face_image, (48,48)).reshape(1,48,48,1)
+	image = image/255
+
+	prediction = model.predict(image)
+	#Double max() because predict's output is a 2D list
+	probability = max(max(prediction))
+	probability = "{:.2f}".format(probability*100)+'%'
+	#Final prediction of the image
+	prediction = np.argmax(prediction)
+
+	return prediction, probability
+
+def main():
+		labels = ['Angry', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+		is_using_program = True
+		camera = cv2.VideoCapture(0)
+
+		while(is_using_program):
+			#Get a single frame from the camera
+			ret, frame = camera.read()
+			#Convert the image into gray scale
+			gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			#Detect face in the image
+			faces = face_classifier.detectMultiScale(gray_image)
+
+			for x, y, w, h in faces:
+				#Draw a rectangle around the detected faces
+				cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+				face_image = gray_image[y:y+h, x:x+w]
+				prediction , probability = get_prediction(face_image)
+				prediction = labels[prediction]+' '+probability
+
+				cv2.putText(frame, prediction, (x,y-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 255, 0), thickness=2)
+			
+			#Show the image with detected faces
+			cv2.imshow("Emotion Detector", frame)
+
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				camera.release()
+				is_using_program = False
+main()
+
+cv2.destroyAllWindows()
+
---
+++ emotion_model.py
@@ -0,0 +1,74 @@
+import tensorflow as tf 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Conv2D, BatchNormalization, MaxPooling2D, Flatten, Dense, Dropout
+import numpy as np
+import matplotlib as plt 
+
+#labels = ['Angry', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+train_path = "data/train+val"
+test_path = "data/test"
+
+#Training set augentation and normalization
+train_datagen = ImageDataGenerator(rescale=1/255,
+							rotation_range=30,
+							shear_range=0.3,
+							zoom_range=0.3,
+							width_shift_range=0.4,
+							height_shift_range=0.4,
+							horizontal_flip=True,
+							fill_mode='nearest')
+
+#Testing set normalization
+test_datagen = ImageDataGenerator(rescale=1/255)
+
+#Get training data "train_path" directory
+train = train_datagen.flow_from_directory(train_path,
+										batch_size=32,
+										target_size=(48,48),
+										shuffle= True)
+
+#Get testing data "test_path" directory
+test = test_datagen.flow_from_directory(test_path,
+										batch_size=32,
+										target_size=(48,48),
+										shuffle= True)
+
+
+model = tf.keras.Sequential([Conv2D(96, (3,3), activation='relu', padding='same', input_shape= (48,48,1)),
+                             BatchNormalization(),
+                             Conv2D(96, (3,3), activation='relu', padding='same'),
+                             BatchNormalization(),
+                             MaxPooling2D(2,2),
+                             
+                             Conv2D(192, (3,3), activation='relu', padding='same'),
+                             BatchNormalization(),
+                             Conv2D(256, (3,3), activation='relu', padding='same'),
+                             BatchNormalization(),
+                             MaxPooling2D(2,2),
+                             
+                             Conv2D(256, (3,3), activation='relu', padding='same'),
+                             BatchNormalization(),
+                             Conv2D(256, (3,3), activation='relu', padding='same'),
+                             BatchNormalization(),
+                             MaxPooling2D(2,2),
+                             
+                             Conv2D(256, (3,3), activation='relu', padding='same'),
+                             BatchNormalization(),
+                             Conv2D(256, (3,3), activation='relu', padding='same'),
+                             
+                             Flatten(),
+                             Dense(256, activation='relu'),
+                             BatchNormalization(),
+                             Dropout(0.25),
+                             Dense(6, activation='softmax')])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+
+#The actually traning was done on Kaggle's servers with a GPU
+model.fit(train ,epochs=150)
+
+model.save('emotion_model_new.h5')
+#This model achieves 71% test accuracy
+model.evaluate(test, verbose=1)
+
